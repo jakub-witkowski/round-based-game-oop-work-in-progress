95c527b1aaf9781527a4bc2583974b61be96c850
Kuba <kuba@MacBook-Air-Kuba.local>
2023-07-19 18:29:26 +0200

    introducing container to hold units

--- player1.cpp
+++ player1.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+
+/* Header files defining classes */
 #include "unit.h"
 #include "base.h"
 #include "knight.h"
@@ -8,6 +11,8 @@
 #include "pikeman.h"
 #include "worker.h"
 #include "ram.h"
+
+/* defining map size */
 #include "map_size.h"
 
 long gold{2000};
@@ -15,15 +20,16 @@ int units_on_the_map_counter{0};
 
 int main()
 {
-    Base* unit_0 = new Base('P', &units_on_the_map_counter);
-    Base* unit_1 = new Base('E', &units_on_the_map_counter);
-    Knight* unit_2 = new Knight('P', &gold, &units_on_the_map_counter);
-    Swordsman* unit_3 = new Swordsman('E', &gold, &units_on_the_map_counter);
-    Catapult* unit_4 = new Catapult('P', &gold, &units_on_the_map_counter);
-    Worker* unit_5 = new Worker('E', &gold, &units_on_the_map_counter);
-    Pikeman* unit_6 = new Pikeman('P', &gold, &units_on_the_map_counter);
-    Archer* unit_7 = new Archer('E', &gold, &units_on_the_map_counter);
-    Ram* unit_8 = new Ram('P', &gold, &units_on_the_map_counter);
+    /* Declaring the vector to hold bases data */
+    std::vector<Base*> bases = {new Base('P', &units_on_the_map_counter), new Base('E', &units_on_the_map_counter)};
+
+    /* Declaring the vector to hold unit data */
+    std::vector<Unit*> units;
+
+    units.push_back(new Knight('P', &gold, &units_on_the_map_counter));
+    units.push_back(new Swordsman('E', &gold, &units_on_the_map_counter));
+
+    //std::cout << "Id of the unit under index 1: " << units[1]->id << std::endl;
 
     return 0;
 }
