035435cd4c9b26b4470bba41ced7a4654c0daddf
Kuba <kuba@MacBook-Air-Kuba.local>
2023-07-22 19:52:28 +0200

    random int generator replaced

--- dice.cpp
+++ dice.cpp
@@ -2,9 +2,14 @@
 
 int dice()
 {
-    std::default_random_engine generator;
+    /*std::default_random_engine generator;
     std::uniform_int_distribution<int> distribution(1,100);
-    auto random_int = std::bind(distribution, generator);
+    auto random_int = std::bind(distribution, generator);*/
 
-    return random_int();
+    std::random_device rd;
+    std::default_random_engine gen(rd());
+    std::uniform_int_distribution<> distr(1, 100); // define the range
+    int dice_roll = distr(gen);
+
+    return dice_roll;
 }
