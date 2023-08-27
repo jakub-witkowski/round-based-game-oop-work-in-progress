977abf4ded3b6c8c83785bd06d8733078711866c
Jakub Witkowski <j.witkowski@poczta.onet.pl>
2023-08-27 16:18:34 +0200

    further work on the map

--- map.cpp
+++ map.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <fstream>
 #include "map.h"
 
 #include <string>
+
+constexpr int Map::line_count(std::string f)
+{
+    std::string line;
+	int line_count{0};
+
+	std::ifstream input(f);   
+
+	if(input.is_open()) 
+	{
+		while(input.peek()!=EOF)
+		{
+			getline(input, line);
+			line_count++;
+		}
+		input.close();
+	}
+ 
+	return line_count;
+}
+
+constexpr int Map::col_count(std::string f)
+{
+    std::string line;
+	int col_count{0};
+
+	std::ifstream input(f);   
+
+	if(input.is_open()) 
+	{
+		getline(input, line);
+		col_count = line.length();		
+	}
+
+	input.close();
+ 
+	return col_count;
+}
 
 void Map::load_map_data(std::string fname)
 {
--- map.h
+++ map.h
@@ -2,18 +2,27 @@
 #define MAP_H
 
 #include <string>
+#include <vector>
 
 class Map
 {
     private:
-    int map_data{};
+    std::vector<std::vector<int>> map_data;
 
     public:
+
+    Map(std::string f)
+    {
+
+    }
+
+    int line_count(std::string);
+
+    int col_count(std::string);
 
     void load_map_data(std::string);
 
     bool can_move_to(int, int);
-
 };
 
 #endif
